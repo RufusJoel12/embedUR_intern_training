6f0609aa9a286a57dfaca88fdf5254310a03d5e7
Rufus_251 <86788675+RufusJoel12@users.noreply.github.com>
2023-02-20 13:29:58 +0530

    Create relational_operations.c

---
+++ module-1/relational_operations.c
@@ -0,0 +1,21 @@
+// Working of relational operators
+#include <stdio.h>
+int main()
+{
+    int a = 2, b = 4, c = 6;
+
+    printf("%d == %d is %d \n", a, b, a == b);
+    printf("%d == %d is %d \n", a, c, a == c);
+    printf("%d > %d is %d \n", a, b, a > b);
+    printf("%d > %d is %d \n", a, c, a > c);
+    printf("%d < %d is %d \n", a, b, a < b);
+    printf("%d < %d is %d \n", a, c, a < c);
+    printf("%d != %d is %d \n", a, b, a != b);
+    printf("%d != %d is %d \n", a, c, a != c);
+    printf("%d >= %d is %d \n", a, b, a >= b);
+    printf("%d >= %d is %d \n", a, c, a >= c);
+    printf("%d <= %d is %d \n", a, b, a <= b);
+    printf("%d <= %d is %d \n", a, c, a <= c);
+
+    return 0;
+}
