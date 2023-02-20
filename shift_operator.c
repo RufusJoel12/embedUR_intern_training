50f2da272731ff1c0ee31a52a0aa47e5bbab74b2
Rufus_251 <86788675+RufusJoel12@users.noreply.github.com>
2023-02-20 13:36:02 +0530

    Delete shift_operator.c

--- shift_operator.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int num=16, i;
-    printf("Right shift by %d: %d\n", i, num >> 1);
-    printf("Left shift by %d: %d\n", i, num << 1);    
-
-    return 0;
-}
