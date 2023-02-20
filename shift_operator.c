7838452e92141a7e30021a1dd013b21962f525f8
Rufus_251 <86788675+RufusJoel12@users.noreply.github.com>
2023-02-20 13:35:53 +0530

    Create shift_operator.c

---
+++ shift_operator.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int num=16, i;
+    printf("Right shift by %d: %d\n", i, num >> 1);
+    printf("Left shift by %d: %d\n", i, num << 1);    
+
+    return 0;
+}
