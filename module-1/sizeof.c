c9321dc2473ebd1ca629721b0e814d6d548c2bc6
Rufus_251 <86788675+RufusJoel12@users.noreply.github.com>
2023-02-20 12:52:01 +0530

    Update sizeof.c

--- module-1/sizeof.c
+++ module-1/sizeof.c
@@ -4,9 +4,9 @@ int main() {
     float floatType;
     double doubleType;
     char charType;
-    printf("Size of int: %zu bytes\n", sizeof(intType));
-    printf("Size of float: %zu bytes\n", sizeof(floatType));
-    printf("Size of double: %zu bytes\n", sizeof(doubleType));
-    printf("Size of char: %zu byte\n", sizeof(charType));  
+    printf("Size of int: %d bytes\n", sizeof(intType));
+    printf("Size of float: %d bytes\n", sizeof(floatType));
+    printf("Size of double: %d bytes\n", sizeof(doubleType));
+    printf("Size of char: %d byte\n", sizeof(charType));  
     return 0;
 }
