8c602157c64cfd4bb879316b53d1291c71c1e23f
Rufus_251 <86788675+RufusJoel12@users.noreply.github.com>
2023-02-23 11:42:43 +0530

    Create bubble_sort.c

---
+++ module-2/bubble_sort.c
@@ -0,0 +1,29 @@
+int main()
+{
+int array[100], n, i, j, swap=0; 
+printf("Enter number of elements ");
+scanf("%d", &n); 
+printf("Enter %d Numbers:", n); 
+for(i = 0; i < n; i++)
+  {
+    scanf("%d", &array[i]); 
+  }
+for(i = 0 ; i < n - 1; i++)
+  {
+    for(j = 0 ; j < n-i-1; j++)
+    {
+       if(array[j] > array[j+1]) 
+         {
+            swap=array[j];
+            array[j]=array[j+1];
+            array[j+1]=swap;
+         }
+    }
+  } 
+printf("Sorted Array:"); 
+for(i = 0; i < n; i++)
+  {
+      printf("%d ", array[i]);
+  }
+return 0;
+}
